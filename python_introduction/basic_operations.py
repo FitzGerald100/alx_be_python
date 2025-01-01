b0ee732e1d49604610f87f9dd196cef523871435
FitzGerald100 <geraldawuzie@gmail.com>
2024-12-31 21:41:56 -0800

    Calculate the sum, difference (by subtracting number2 from number1), and product of number1 and number2.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+sum_num = number1 + number2
+print("Additions of", number1, "and", number2, "is", sum_num)
+
+sub_num = number1 - number2
+print("Substraction of", number1, "and", number2, "is", sub_num)
+
+
+mult_num = number1 * number2
+print("multiplication of", number1, "and", number2, "is", mult_num)
