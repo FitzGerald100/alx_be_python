170534d0a5c797d1b1149aa2ed5d7c02857fec28
FitzGerald100 <geraldawuzie@gmail.com>
2025-01-05 01:37:13 -0800

    adding time to thr format

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,8 +3,7 @@ principal = 1000, rate = 0.05, and time = 3
 I = interest
 p = principal
 r = rate
+t = time
  I = (P * R * T)
-
-principal = 1000, rate = 0.05, and time = 3
 
 print("The simple interest is" I)
