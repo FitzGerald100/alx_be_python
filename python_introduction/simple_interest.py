34a7a1d7663243d8e350614210b85d0dedd30990
FitzGerald100 <geraldawuzie@gmail.com>
2025-01-05 01:43:52 -0800

    corrections

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
 principal = 1000, rate = 0.05, and time = 3
 
 I = interest
-p = principal
-r = rate
-t = time
+P = principal
+R = rate
+T = time
  I = (P * R * T)
 
 print("The simple interest is", I)
