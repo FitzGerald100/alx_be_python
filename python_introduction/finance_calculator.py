beab6513fdf27f97aa3c8c4af8f2c8c1a7be148f
FitzGerald100 <geraldawuzie@gmail.com>
2025-01-05 02:57:26 -0800

    calculated finance

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,10 @@
+monthly income = int(input("enter your monthly income "))
+total monthly expenses = int(input("enter your total monthly expenses "))
+Monthly Savings = monthly income - total monthly expenses
+annual interest rate = 5%
+Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05)
+monthly income = $5000
+total monthly expenses = $4000
+print("Your monthly savings are" , Monthly Savings)
+print("Projected savings after one year, with interest, is:" , Project Savings.)
+
