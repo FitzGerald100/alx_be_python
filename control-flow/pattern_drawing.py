711edacfefee50f9e1e755ff3990656466f34082
FitzGerald100 <geraldawuzie@gmail.com>
2025-01-10 03:27:40 -0800

    mee

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,7 @@
+size = int(input("Enter the size of the pattern: ")
+row = 0
+while row < size:
+           for col in range(ssize):
+           print("*", end="")
+        print()
+        row += 1
