899ac051649e3bcdcbb026c4a05280c72f73d3e4
FitzGerald100 <geraldawuzie@gmail.com>
2025-01-16 14:32:07 +0100

    main.py

---
+++ fns_and_dsa/main.py
@@ -0,0 +1,6 @@
+Arithmetic Operations
+Enter the first number: 5
+Enter the second number: 0
+Enter the operation (add, subtract, multiply, divide): divide
+Result: Error: Division by zero is undefined.
+
