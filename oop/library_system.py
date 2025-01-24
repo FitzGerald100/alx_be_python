8df41bba0a6c83c175d1ee7936f04de2b30051f2
FitzGerald100 <133219312+FitzGerald100@users.noreply.github.com>
2025-01-24 13:23:39 +0100

    first commit

---
+++ oop/library_system.py
@@ -0,0 +1,34 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}"
+    
+class Ebook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, File Size: {self.file_size}"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Page Count: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.book.append(book)
+        else:
+            print("only books can be added to the library")
+    def 
